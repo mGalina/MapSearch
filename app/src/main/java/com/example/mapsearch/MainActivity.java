fd1d6d8a8cd167f288ddf28d1442a224f02727db
Galina Marakina <g.marakina.2gis@gmail.com>
2020-07-04 16:31:30 +0300

    No commit

--- app/src/main/java/com/example/mapsearch/MainActivity.java
+++ app/src/main/java/com/example/mapsearch/MainActivity.java
@@ -12,8 +12,6 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText address;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        address = findViewById(R.id.et_address);
+        EditText address = findViewById(R.id.et_address);
         Button search = findViewById(R.id.btn_search);
 
         final String text = address.getText().toString();
@@ -31,34 +29,24 @@ public class MainActivity extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent openMap = new Intent();
+                openMap.setAction(Intent.ACTION_VIEW);
                 if (text.length() > 0 && Character.isLetter(text.charAt(0))) {
-                    searchByCoordinates();
+                    Uri uri = Uri.parse("geo:55.704968, 69.625206");
+                    openMap.setData(uri);
+                    if (openMap.resolveActivity(getPackageManager()) != null) {
+                        startActivity(openMap);
+                    } else {
+                        Log.d("Intent", "Browser not found");
+                    }
                 } else {
-                    searchByName();
-                }
-            }
-
-            private void searchByCoordinates() {
-                Intent openMap = new Intent();
-                openMap.setAction(Intent.ACTION_VIEW);
-                Uri uri = Uri.parse("geo:55.704968, 69.625206");
-                openMap.setData(uri);
-                if (openMap.resolveActivity(getPackageManager()) != null) {
-                    startActivity(openMap);
-                } else {
-                    Log.d("Intent", "Browser not found");
-                }
-            }
-
-            private void searchByName() {
-                Intent openMap = new Intent();
-                openMap.setAction(Intent.ACTION_VIEW);
-                Uri uri = Uri.parse("geo:?q=Moscow");
-                openMap.setData(uri);
-                if (openMap.resolveActivity(getPackageManager()) != null) {
-                    startActivity(openMap);
-                } else {
-                    Log.d("Intent", "Browser not found");
+                    Uri uri = Uri.parse("geo:?q=Moscow");
+                    openMap.setData(uri);
+                    if (openMap.resolveActivity(getPackageManager()) != null) {
+                        startActivity(openMap);
+                    } else {
+                        Log.d("Intent", "Browser not found");
+                    }
                 }
             }
         });
